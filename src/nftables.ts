ffd8c10af06c48a4f7606044d27fc91977081c05
Koushik Dutta <koushd@gmail.com>
2025-02-23 23:13:34 -0800

    fix casing

--- src/nftables.ts
+++ src/nftables.ts
@@ -1,5 +1,4 @@
 export function generateNftablesConf(pairs: {
-    nativeId: string,
     ipVersion: 'ip' | 'ip6',
     wanInterface: string,
     lanInterface: string
@@ -7,33 +6,20 @@ export function generateNftablesConf(pairs: {
     let config = '';
 
     pairs.forEach(pair => {
-        const { ipVersion, nativeId, wanInterface, lanInterface } = pair;
+        const { ipVersion, wanInterface, lanInterface } = pair;
 
         config += `
 table ${ipVersion} nat {
     chain POSTROUTING {
-        type nat hook postrouting priority 100;
-        oifname "${wanInterface}" jump ${nativeId}
-    }
-
-    chain ${nativeId} {
-        oifname "${wanInterface}" masquerade
+        type nat hook postrouting priority 100; policy accept;
+        oif "${wanInterface}" masquerade
     }
 }
-
-# Create the filter table and chain
-table ip filter {
-    chain ${nativeId} {
-        # Accept traffic from ${lanInterface} to ${wanInterface}
-        iifname "${lanInterface}" oifname "${wanInterface}" accept
-
-        # Accept related/established traffic from ${wanInterface} to ${lanInterface}
-        iifname "${wanInterface}" oifname "${lanInterface}" ct state related,established accept
-    }
-
+table ${ipVersion} filter {
     chain FORWARD {
-        type filter hook forward priority 0;
-        jump ${nativeId}
+        type filter hook forward priority 0; policy drop;
+        iif "${lanInterface}" oif "${wanInterface}" accept
+        iif "${wanInterface}" oif "${lanInterface}" ct state established,related accept
     }
 }
 `;
