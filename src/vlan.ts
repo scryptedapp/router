20efb1c42ef6105f96018760590faf895c6283dc
Koushik Dutta <koushd@gmail.com>
2025-02-26 22:05:40 -0800

    fix multiple dhcp server instances binding to lo

--- src/vlan.ts
+++ src/vlan.ts
@@ -284,7 +284,7 @@ After=network.target
 User=root
 Group=root
 Type=simple
-ExecStart=dnsmasq -d -R -i ${interfaceName} -z ${dhcpRanges.map(d => `--dhcp-range=${d}`).join(' ')} --dhcp-option=6,${addressWithoutMask} ${serverArgs.join(' ')}
+ExecStart=dnsmasq -d -R -i ${interfaceName} --except-interface=lo -z ${dhcpRanges.map(d => `--dhcp-range=${d}`).join(' ')} --dhcp-option=6,${addressWithoutMask} ${serverArgs.join(' ')}
 Restart=always
 RestartSec=3
 StandardOutput=null
