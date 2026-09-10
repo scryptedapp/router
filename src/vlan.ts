7a4479f7742665d2c8c3ff90442388813fc23280
Koushik Dutta <koushd@gmail.com>
2026-09-10 11:52:07 -0700

    start dnsmasq in foreground, -d is for debugging and hangs on tcp

--- src/vlan.ts
+++ src/vlan.ts
@@ -788,7 +788,7 @@ After=network.target
 User=root
 Group=root
 Type=simple
-ExecStart=dnsmasq -d -R -h ${enableRouterAdvertisements} --host-record=${os.hostname()},${addressWithoutMask} -i ${interfaceName} --except-interface=lo -z ${dhcpRanges.map(d => `--dhcp-range=${d}`).join(' ')} ${dnsSearchDomains} --dhcp-option=3,${dhcpGateway} --dhcp-option=6,${addressWithoutMask} ${serverArgs.join(' ')} --dhcp-leasefile=${this.leaseFile} --dhcp-hostsfile=${hostsFile}
+ExecStart=dnsmasq -k -R -h ${enableRouterAdvertisements} --host-record=${os.hostname()},${addressWithoutMask} -i ${interfaceName} --except-interface=lo -z ${dhcpRanges.map(d => `--dhcp-range=${d}`).join(' ')} ${dnsSearchDomains} --dhcp-option=3,${dhcpGateway} --dhcp-option=6,${addressWithoutMask} ${serverArgs.join(' ')} --dhcp-leasefile=${this.leaseFile} --dhcp-hostsfile=${hostsFile}
 Restart=always
 RestartSec=3
 #StandardOutput=null
